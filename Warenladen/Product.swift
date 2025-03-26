05e5cecb28796e2fda33bcbfa47038298cef32f6
Rylie Castell <rylie.castell@gmail.com>
2025-03-26 15:09:21 +0100

    add product struct

---
+++ Warenladen/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  Warenladen
+//
+//  Created by Rylie Castell on 26.03.25.
+//
+
+import Foundation
+
+// Aufgabe 2.1 Produkt modellieren
+
+struct Product {
+    let name: String
+    let price: Double
+    let description: String
+}
